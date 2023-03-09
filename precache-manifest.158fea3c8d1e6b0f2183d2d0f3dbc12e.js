ef603128757e317637f847493b1841348fa0a2de
Natan Barbosa <natanbarbosa15@gmail.com>
2023-03-08 22:29:00 -0300

    Updates

---
+++ precache-manifest.158fea3c8d1e6b0f2183d2d0f3dbc12e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8f565fe13ca1ffbe0f032988f8c5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2fe92c483e3ec8b8831",
+    "url": "/static/js/2.c4e5640e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4e5640e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c35332e18f75239e9f75",
+    "url": "/static/js/main.5894d41f.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "91810c7fef7162974a73238fd5ffb45c",
+    "url": "/static/media/profile.91810c7f.jpg"
+  }
+]);
